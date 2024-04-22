061b36aaabc9e98cbfe8247b001cae11c3ca5e58
AlbertoMessineo <albevorox@gmail.com>
2024-04-22 19:32:51 +0200

    feat: deal button no longer keeps generating more then two cards

--- main.js
+++ main.js
@@ -3,19 +3,26 @@ document.getElementById("allIn").onclick = function () {
   document.getElementById("chips").classList.toggle("align-self-end");
   document.getElementById("chips").classList.toggle("align-self-center");
 };
-
+;
 //funzione che crea le carte coperte in mano al giocatore
-document.getElementById("deal").onclick = function () {
-  for (let i = 0; i < 2; i++) {
-    const div = document.createElement("div");
-    div.className = "back-card";
-    const img = document.createElement("img");
-    img.src = "img/red-back.png";
-    img.alt = "back-card";
-    div.appendChild(img);
-    document.getElementById("cards-back").appendChild(div);
+let dealButtonClicked = false;
+
+document.getElementById("deal").onclick = function() {
+  if (!dealButtonClicked) {
+    for (let i = 0; i < 2; i++) {
+      const div = document.createElement("div");
+      div.className = "back-card";
+      const img = document.createElement("img");
+      img.src = "img/red-back.png";
+      img.alt = "back-card";
+      div.appendChild(img);
+      document.getElementById("cards-back").appendChild(div);
+    }
+    dealButtonClicked = true; // Imposta il flag a true dopo il primo click
   }
 };
+
+
 // Faccio una chiamata API per ottenere le immagini delle carte
 fetch("https://www.deckofcardsapi.com/api/deck/new/draw/?count=52")
   .then((response) => response.json())
